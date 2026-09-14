28427e60053fc8bfd090d509e6f5a739d00f5ad4
Manish Kumar <manishyadav00772@gmail.com>
2026-09-14 15:17:34 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Min Cost to Make Size 1/min-cost-to-make-size-1.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int cost(int[] arr) {
+        // code here
+        int min = arr[0];
+        for(int i=0; i<arr.length; i++){
+            if(arr[i] <min){
+                min = arr[i];
+            }
+        }
+        return (arr.length -1) * min;
+    }
+}
